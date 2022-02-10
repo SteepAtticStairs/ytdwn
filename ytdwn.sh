5870e43860ef7624e588822c224a4d3fe1f59da6
Andrew Lawlor <lawlorcube06@gmail.com>
2022-02-09 20:42:00 -0500

    added support for a wider variety of formats

--- ytdwn.sh
+++ ytdwn.sh
@@ -21,29 +21,109 @@ read filePath
     elif [[ $filePath == documents ]]; then
         cd $HOME/Documents
     elif [[ $filePath != desktop ]] || [[ $filePath != downloads ]] || [[ $filePath != documents ]]; then
-	cd "$filePath"
+        cd "$filePath"
     fi
 
 pwd
 
 echo " "
-echo "Would you like to download the video to mp3 or mp4, or CHECK avaliable formats?"
+echo "Would you like to download the video to mp3 (audio), mp4 (video), the BEST video + audio format available (generally webm), a specific format NUMBER, or CHECK avaliable formats?"
 read format
 
-if [ $format = mp4 ]; then
+if [ $format == mp4 ]; then
     echo " "
     echo "What quality would you like to download the video in?"
     echo "You can chose 1080, 720, 480, 360, 240, 144"
     read quality
 
+    echo " "
+    echo "Would you like to add a cookies file? Type NO if no, if yes, please provide the filepath to the cookies file."
+    read cookies
+
+    if [ $cookies == no ]; then
+
     yt-dlp -v -S 'res:'$quality'' --format mp4 $link
 
-elif [ $format = mp3 ]; then
+    elif [ $cookies != no ]; then
+
+    yt-dlp --cookies $cookies -v -S 'res:'$quality'' --format mp4 $link
+
+    fi
+
+##########################
+
+elif [ $format == mp3 ]; then
+
+    echo " "
+    echo "Would you like to add a cookies file? Type NO if no, if yes, please provide the filepath to the cookies file."
+    read cookies
+
+    if [ $cookies == no ]; then
 
     yt-dlp -f 'ba' -x --audio-format mp3 $link
 
-elif [ $format = check ]; then
+    elif [ $cookies != no ]; then
+
+    yt-dlp --cookies $cookies -f 'ba' -x --audio-format mp3 $link
+
+    fi
+
+##########################
+    
+elif [ $format == best ]; then
+
+    echo " "
+    echo "Would you like to add a cookies file? Type NO if no, if yes, please provide the filepath to the cookies file."
+    read cookies
+
+    if [ $cookies == no ]; then
+
+    yt-dlp -f "bestvideo+bestaudio" $link
+
+    elif [ $cookies != no ]; then
+
+    yt-dlp --cookies $cookies -f "bestvideo+bestaudio" $link
+
+    fi
+
+##########################
+
+elif [ $format == check ]; then
+
+    echo " "
+    echo "Would you like to add a cookies file? Type NO if no, if yes, please provide the filepath to the cookies file."
+    read cookies
+
+    if [ $cookies == no ]; then
 
     yt-dlp -F $link
+
+    elif [ $cookies != no ]; then
+
+    yt-dlp --cookies $cookies -F $link
+
+    fi
+
+##########################
+
+elif [ $format == number ]; then
+
+    echo " "
+    echo "What format number would you like to download?"
+    read formatNumber
+
+    echo " "
+    echo "Would you like to add a cookies file? Type NO if no, if yes, please provide the filepath to the cookies file."
+    read cookies
+
+    if [ $cookies == no ]; then
+
+    yt-dlp -f $formatNumber $link
+
+    elif [ $cookies != no ]; then
+
+    yt-dlp --cookies $cookies -f $formatNumber $link
+
+    fi
 
 fi
